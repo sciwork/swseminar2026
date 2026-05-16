7e3943c8ecd629b5c2ca24a237338dedb3b68a23
5x <longhorn482653@gmail.com>
2026-05-16 16:58:13 +0800

    fix(constants): correct SCHEDULE_URL

--- configurations/constants.ts
+++ configurations/constants.ts
@@ -3,10 +3,9 @@ export const COOKIE_SHOW_COOKIE_BANNER_KEY = "__sciwork_show_cookie_banner";
 export const COOKIE_IS_COOKIE_CONSENTED_KEY = "__sciwork_is_cookie_consented";
 export const DEFAULT_LOCALE = "en";
 export const UNKNOWN_LOCALE = "";
-export const REGISTER_URL =
-  "https://sciwork.kktix.cc/events/seminar2026";
+export const REGISTER_URL = "https://sciwork.kktix.cc/events/seminar2026";
 export const CFP_URL = "https://pretalx.sciwork.dev/sw25/submit";
 export const SCHEDULE_URL =
-  "https://pretalx.sciwork.dev/sw25/schedule/export/schedule.json";
+  "https://pretalx.sciwork.dev/seminar26/schedule/export/schedule.json";
 export type CookieConsentValue = "accepted" | "rejected" | undefined;
 export const COOKIE_NAME = "cookieConsent";
